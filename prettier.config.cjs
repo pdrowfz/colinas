84006ef0db41b85f9ff3d17684416117ae5d3fcc
Pedro Zanetti <pdrowfz@gmail.com>
2023-04-09 17:00:50 -0300

    feat: add lint configs, lint-staged and lint-fix everything

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,6 +1,9 @@
 /** @type {import("prettier").Config} */
 const config = {
-  plugins: [require.resolve("prettier-plugin-tailwindcss")],
+  plugins: [require.resolve('prettier-plugin-tailwindcss')],
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'all',
 };
 
 module.exports = config;
