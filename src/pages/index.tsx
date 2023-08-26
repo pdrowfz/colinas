48a93bc3fe8dde79957449e4aa84cb383e6f5764
Pedro Zanetti <pdrowfz@gmail.com>
2023-08-26 00:04:26 -0300

    feat: style login page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,10 @@
 import { SignInButton, useUser } from '@clerk/nextjs';
 import { type NextPage } from 'next';
-import { type ReactNode } from 'react';
 import { useRouter } from 'next/router';
-
-const Button = ({ children }: { children: ReactNode }) => {
-  return <button className="border px-4 py-2">{children}</button>;
-};
+import { ColinasLogo } from '~/components/ColinasLogo';
+import { LoadingSpinner } from '~/components/Loading';
+import { Button } from '~/components/ui/button';
+import { Separator } from '~/components/ui/separator';
 
 const Login: NextPage = () => {
   const user = useUser();
@@ -16,21 +15,22 @@ const Login: NextPage = () => {
   }
 
   return (
-    <>
-      <div className="flex h-screen justify-center">
-        <main className="h-full w-full border-x border-slate-400 md:max-w-5xl">
-          <div className="flex justify-center">
-            {!user.isSignedIn && (
-              <div className="flex items-center justify-center pt-48">
-                <Button>
-                  <SignInButton />
-                </Button>
-              </div>
-            )}
-          </div>
-        </main>
+    <div className="flex flex-row overflow-hidden">
+      <div className="h-screen w-1/2 bg-[url('/assets/images/sofi.jpeg')] bg-cover bg-center bg-no-repeat"></div>
+      <div className="flex h-screen w-1/2 flex-col items-center justify-center bg-slate-200">
+        <div className="flex flex-row items-center justify-center text-xl font-medium text-slate-900">
+          <ColinasLogo className="h-20 w-20 fill-slate-900" />
+          Colinas Pick&apos;em
+        </div>
+        <Separator className="mb-4 w-3/4 bg-slate-700" />
+        {!user.isLoaded ? <LoadingSpinner size={32} /> : null}
+        {user.isLoaded && !user.isSignedIn && (
+          <Button size="lg">
+            <SignInButton />
+          </Button>
+        )}
       </div>
-    </>
+    </div>
   );
 };
 
