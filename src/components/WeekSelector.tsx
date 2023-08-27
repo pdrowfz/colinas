47943a0a70149fb4d76b7d3e2ff405fdb35b0844
Pedro Zanetti <pdrowfz@gmail.com>
2023-08-27 00:53:51 -0300

    feat: improve week selector

--- src/components/WeekSelector.tsx
+++ src/components/WeekSelector.tsx
@@ -10,11 +10,12 @@ const WeekSelector = ({ week, setWeek }: WeekSelectorProps) => {
   return (
     <div className="mb-8 flex flex-col items-center">
       <h3 className="text-2xl font-semibold text-slate-900">Week {week}</h3>
-      <div className="mt-8 flex flex-row items-center gap-2">
+      <div className="mt-8 flex flex-row items-center gap-0 md:gap-1 lg:gap-2">
         {weeks.map((weekObj) => (
           <Button
             key={weekObj.week}
             variant={weekObj.week == week ? 'default' : 'secondary'}
+            size="sm"
             onClick={() => setWeek(weekObj.week)}
           >
             {weekObj.week}
